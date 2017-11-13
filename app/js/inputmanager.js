581b4a3440712266fa632dc51b255e2a3c804174
taku5151 <taku8017@gmail.com>
2017-11-13 02:16:59 -0800

    lots of updates

---
+++ app/js/inputmanager.js
@@ -0,0 +1,15 @@
+class InputManager {
+  down(event) {
+    switch(event.keyCode) {
+      case 27: //Escape
+        skutextbox.value = "";
+        resetInput();
+        break;
+      case 13: //Enter
+        executequery();
+        break;
+      default:
+        break;
+    }
+  }
+}
